03826599fe3f32d9ab12df3a19e4778108556acc
elpablii <pablo.villarroel01@alumnos.ucn.cl>
2026-01-21 15:54:38 -0300

    instalación de next.js y sus dependencias para la página piloto cidere

---
+++ practicaprofesional/cidere/paginapilotocidere/src/app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Cidere Biobío</h1>
+      <p className="mt-4 text-xl">Sitio en construcción</p>
+    </div>
+  );
+}
